93ea1ac2b95b1beda28a1b07ff1aad1a2741df18
Jefferson M Souza <jefferson.souza@synchro.com.br>
2018-03-02 22:39:10 -0300

    webpack plugins working fine

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,32 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptmizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const cssnano = require('cssnano');
+
+let plugins = [];
+const isProd = process.env.NODE_ENV == 'production';
+
+if (isProd) {
+  plugins.push(
+    new OptmizeCssAssetsPlugin({
+      cssProcessor: cssnano,
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true
+        }
+      }
+    })
+  );
+}
+
+plugins.push(new ExtractTextPlugin('styles.css'));
 
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist',
+    publicPath: 'dist'
   },
   module: {
     rules: [
@@ -13,29 +34,33 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader',
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader',
+          fallback: 'style-loader'
+        })
       },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader',
+        loader: 'file-loader'
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
-      },
-    ],
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
   },
+  plugins: plugins
 };
