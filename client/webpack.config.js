beb96ec8f50d381c4079dd944e706f03045ae4e8
Jefferson M Souza <jefferson.souza@synchro.com.br>
2018-03-05 22:37:20 -0300

    css and js are injected into html file automatically

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptmizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 const cssnano = require('cssnano');
 const webpack = require('webpack');
 
@@ -13,20 +14,32 @@ if (isProd) {
       cssProcessor: cssnano,
       cssProcessorOptions: {
         discardComments: {
-          removeAll: true,
-        },
-      },
+          removeAll: true
+        }
+      }
     })
   );
 
   plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
 }
 
+plugins.push(
+  new HtmlPlugin({
+    minify: {
+      html5: true,
+      collapseWhitespace: true,
+      removeComments: true
+    },
+    hash: true,
+    filename: 'index.html',
+    template: `${__dirname}/main.html`
+  })
+);
 plugins.push(new ExtractTextPlugin('styles.css'));
 plugins.push(
   new webpack.ProvidePlugin({
     $: 'jquery',
-    jQuery: 'jquery',
+    jQuery: 'jquery'
   })
 );
 
@@ -34,8 +47,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
@@ -43,46 +55,46 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           use: 'css-loader',
-          fallback: 'style-loader',
-        }),
+          fallback: 'style-loader'
+        })
       },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader',
+        loader: 'file-loader'
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
-      },
-    ],
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
   },
   plugins: plugins,
   optimization: {
     minimize: isProd,
-    //runtimeChunk: true,
+    runtimeChunk: true,
     splitChunks: {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendor',
-          chunks: 'all',
-        },
-      },
-    },
-  },
+          chunks: 'all'
+        }
+      }
+    }
+  }
 };
